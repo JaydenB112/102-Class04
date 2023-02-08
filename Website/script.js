7fea7ee55f36f1f51317a27281f88317741098e4
Jayden Boyd <jaydenboydb1@gmail.com>
2023-02-08 15:05:09 -0600

    empty

---
+++ Website/script.js
@@ -0,0 +1,25 @@
+let a =1;
+
+function l2() {
+    let b=5;
+    console.log(a)
+    console.log(b + a)
+}
+
+let lesson = function (x) {
+    let b = 2;
+    
+    console.log(x +b);
+}
+
+lesson(a);
+
+console.log(b);
+
+function whodat() {
+    let name = prompt("What is your name?");
+    if(name !='Jay') {
+        alert("error");
+    } else {}
+         alert("Hello, " + name);
+}
